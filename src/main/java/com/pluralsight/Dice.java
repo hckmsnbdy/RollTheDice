8f362d1851a3c8b5447c53356eb52fa1c13b285b
hckmsnbdy <hckmsnbdy@gmail.com>
2025-10-02 11:17:16 -0400

    Working on while loop.

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,6 +1,24 @@
 package com.pluralsight;
 
 public class Dice {
-    public void roll(){
+    public int roll(){
+
+        int randomNumber = (int)(Math.random() * 6) + 1;
+
+        return randomNumber;
+    }
+
+    public static void main(String[] args) {
+
+       int i = 0;
+
+       while(i<=100){
+
+
+           i+=1;
+
+           System.out.println("The roll number is: " + new Dice().roll());
+       }
+
     }
 }
