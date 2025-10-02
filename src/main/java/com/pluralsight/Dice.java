f39d6166c0e6652e25acad49beb3f63eb4fd72c9
hckmsnbdy <hckmsnbdy@gmail.com>
2025-10-02 12:59:37 -0400

    Done.

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,24 +1,7 @@
 package com.pluralsight;
 
 public class Dice {
-    public int roll(){
-
-        int randomNumber = (int)(Math.random() * 6) + 1;
-
-        return randomNumber;
-    }
-
-    public static void main(String[] args) {
-
-       int i = 0;
-
-       while(i<=100){
-
-
-           i+=1;
-
-           System.out.println("The roll number is: " + new Dice().roll());
-       }
-
+    public int roll() {
+        return (int)(Math.random() * 6) + 1;
     }
 }
